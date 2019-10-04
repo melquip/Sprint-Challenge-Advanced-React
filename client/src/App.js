a648eb91e5913f6930e1cafb8ad3f0522eb8a0a1
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-04 08:20:54 +0100

    Added request to the api and displayed the response data

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import Player from './Player';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			players: []
+		}
+	}
+	componentDidMount() {
+		axios.get('http://localhost:5000/api/players')
+			.then((response) => {
+				this.setState({ players: response.data });
+			})
+			.catch((error) => console.error(error))
+	}
+	render() {
+		const { players } = this.state;
+		return (
+			<div className="App" >
+				<h1>Players</h1>
+				<div className="players">
+					{
+						players.map(player => <Player key={player.id} player={player} />)
+					}
+				</div>
+			</div>
+		);
+	}
 }
-
-export default App;
